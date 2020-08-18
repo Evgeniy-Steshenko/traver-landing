ad8551ed0b50fe3025b5080afa112495f0d39609
Evgeniy-Steshenko <evgeniy.steshenko.1999@mail.ru>
2020-08-19 00:36:35 +0300

    Image

---
+++ src/components/Destinations.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="destinations">
+                <div class="destinations-text">
+                    <h1>Featured destinations</h1>
+                    <p>View all</p>
+                </div>
+                <div class="destinations-images">
+                    <div class="destinations-image1">
+                        <h4>Raja Ampat</h4>
+                        <p>Indonesia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
